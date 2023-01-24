56ac77dd7d49cf197ca4fa1039d70f7fb4cac44c
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-01-24 22:31:24 +0700

    feat: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,46 +117,48 @@ export default function Home() {
       </section>
       <section className="hits">
         <div className="hits__container">
-          <h2 className="hits__title">Хиты продаж</h2>
-          <div>
-            <Swiper
-              spaceBetween={20}
-              slidesPerView={8}
-              slidesPerGroup={4}
-              loopFillGroupWithBlank={true}
-              loop={true}
-              pagination={{
-                clickable: true,
-              }}
-              navigation={true}
-              modules={[Pagination, Navigation]}
-              className="hits__swiper"
-            >
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 5</SwiperSlide>
-              <SwiperSlide>Slide 6</SwiperSlide>
-              <SwiperSlide>Slide 7</SwiperSlide>
-              <SwiperSlide>Slide 8</SwiperSlide>
-              <SwiperSlide>Slide 9</SwiperSlide>
-              <SwiperSlide>Slide 10</SwiperSlide>
-              <SwiperSlide>Slide 11</SwiperSlide>
-              <SwiperSlide>Slide 12</SwiperSlide>
-              <SwiperSlide>Slide 13</SwiperSlide>
-              <SwiperSlide>Slide 14</SwiperSlide>
-              <SwiperSlide>Slide 15</SwiperSlide>
-              <SwiperSlide>Slide 16</SwiperSlide>
-              <SwiperSlide>Slide 17</SwiperSlide>
-              <SwiperSlide>Slide 18</SwiperSlide>
-              <SwiperSlide>Slide 19</SwiperSlide>
-              <SwiperSlide>Slide 20</SwiperSlide>
-              <SwiperSlide>Slide 21</SwiperSlide>
-              <SwiperSlide>Slide 22</SwiperSlide>
-              <SwiperSlide>Slide 23</SwiperSlide>
-              <SwiperSlide>Slide 24</SwiperSlide>
-            </Swiper>
+          <div className="hits__offset">
+            <h2 className="hits__title">Хиты продаж</h2>
+            <div>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={8}
+                slidesPerGroup={8}
+                loopFillGroupWithBlank={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="hits__swiper"
+              >
+                <SwiperSlide className="hits__slide">Slide 1</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 2</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 3</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 4</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 5</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 6</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 7</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 8</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 9</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 10</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 11</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 12</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 13</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 14</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 15</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 16</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 17</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 18</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 19</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 20</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 21</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 22</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 23</SwiperSlide>
+                <SwiperSlide className="hits__slide">Slide 24</SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
@@ -164,6 +166,46 @@ export default function Home() {
         <div className="stickers__container">
           <div className="stickers__offset">
             <h2 className="stickers__title">Стикеры</h2>
+            <div>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={8}
+                slidesPerGroup={8}
+                loopFillGroupWithBlank={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="stickers__swiper"
+              >
+                <SwiperSlide className="stickers__slider">Slide 1</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 2</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 3</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 4</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 5</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 6</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 7</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 8</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 9</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 10</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 11</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 12</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 13</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 14</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 15</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 16</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 17</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 18</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 19</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 20</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 21</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 22</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 23</SwiperSlide>
+                <SwiperSlide className="stickers__slider">Slide 24</SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
@@ -171,6 +213,46 @@ export default function Home() {
         <div className="key-chains__container">
           <div className="key-chains__offset">
             <h2 className="key-chains__title">Брелки</h2>
+            <div>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={8}
+                slidesPerGroup={8}
+                loopFillGroupWithBlank={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="key-chains__swiper"
+              >
+                <SwiperSlide className="key-chains__slider">Slide 1</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 2</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 3</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 4</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 5</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 6</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 7</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 8</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 9</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 10</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 11</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 12</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 13</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 14</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 15</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 16</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 17</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 18</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 19</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 20</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 21</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 22</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 23</SwiperSlide>
+                <SwiperSlide className="key-chains__slider">Slide 24</SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
@@ -178,6 +260,46 @@ export default function Home() {
         <div className="notebooks__container">
           <div className="notebooks__offset">
             <h2 className="notebooks__title">Тетради</h2>
+            <div>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={8}
+                slidesPerGroup={8}
+                loopFillGroupWithBlank={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="notebooks__swiper"
+              >
+                <SwiperSlide className="notebooks__slider">Slide 1</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 2</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 3</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 4</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 5</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 6</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 7</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 8</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 9</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 10</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 11</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 12</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 13</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 14</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 15</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 16</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 17</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 18</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 19</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 20</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 21</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 22</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 23</SwiperSlide>
+                <SwiperSlide className="notebooks__slider">Slide 24</SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
@@ -185,6 +307,46 @@ export default function Home() {
         <div className="mugs__container">
           <div className="mugs__offset">
             <h2 className="mugs__title">Кружка</h2>
+            <div>
+              <Swiper
+                spaceBetween={20}
+                slidesPerView={8}
+                slidesPerGroup={8}
+                loopFillGroupWithBlank={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mugs__swiper"
+              >
+                <SwiperSlide className="mugs__slider">Slide 1</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 2</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 3</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 4</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 5</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 6</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 7</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 8</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 9</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 10</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 11</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 12</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 13</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 14</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 15</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 16</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 17</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 18</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 19</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 20</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 21</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 22</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 23</SwiperSlide>
+                <SwiperSlide className="mugs__slider">Slide 24</SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
