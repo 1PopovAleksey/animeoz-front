be2d0399bfbaf237b5e1d98111c36b2879e1c419
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-02-07 20:44:39 +0700

    feat: add NewView

---
+++ src/views/NewView.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="new">
+    <div class="new__container">
+      <div class="new__offset">
+        <div class="filter">
+          <h2>Фильтр</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: "Naming",
+          catagery: "cup",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
