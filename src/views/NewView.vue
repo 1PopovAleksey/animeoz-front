7024f046634ac4b1254de15804e2cf7fbcadfb42
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-02-14 01:06:28 +0300

    feat: update NewView

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -4,6 +4,12 @@
       <div class="new__offset">
         <div class="filter">
           <h2>Фильтр</h2>
+          <ProductCard
+            v-for="item in items"
+            :key="item.id"
+            :name="item.name"
+            :category="item.category"
+          />
         </div>
       </div>
     </div>
@@ -11,18 +17,20 @@
 </template>
 
 <script>
+import axios from "axios";
+import ProductCard from "../components/ProductCard.vue";
+
 export default {
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          name: "Naming",
-          catagery: "cup",
-        },
-      ],
+      items: [],
     };
   },
+  mounted() {
+    axios.get("http://127.0.0.1:3000/new").then((res) => (this.items = res));
+    console.log(this.items);
+  },
+  components: { ProductCard },
 };
 </script>
 
