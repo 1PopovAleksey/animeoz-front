07ba12f38efa2294bdaf3e5eb8ad6d75314d6477
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-02-14 01:06:05 +0300

    feat: add ProductCard

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <p>{{ category }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: { name: String, category: String },
+};
+</script>
+
+<style lang="scss" scoped></style>
